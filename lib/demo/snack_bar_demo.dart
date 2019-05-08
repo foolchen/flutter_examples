39f24b8d3ee88debab2a601c8ef8c6746378cadd
foolchen <chenchong8806@gmail.com>
2019-05-08 15:34:17 +0800

    增加了SnackBar示例

---
+++ lib/demo/snack_bar_demo.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_examples/widget/snack_bar.dart';
+
+void main() => runApp(SnackApp());
+
+class SnackApp extends StatelessWidget {
+  GlobalKey<SnackBarWidgetState> _globalKey = GlobalKey();
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("SnackDemo"),
+            actions: <Widget>[
+              InkWell(
+                child: Padding(
+                  child: Center(
+                    child: Text("显示"),
+                  ),
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                ),
+                onTap: () {
+                  _globalKey.currentState
+                      .showSnackBar("这是SnackBar count: ${count++}");
+                },
+              ),
+              Padding(
+                child: InkWell(
+                  child: Padding(
+                    child: Center(
+                      child: Text("隐藏"),
+                    ),
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                  ),
+                  onTap: () {
+                    _globalKey.currentState.dismiss();
+                  },
+                ),
+                padding: EdgeInsets.only(right: 10),
+              )
+            ],
+          ),
+          body: SnackBarWidget(
+              key: _globalKey,
+              textBuilder: (String message) {
+                return Text(message ?? "");
+              },
+              content: Text("这是内容部分")),
+        ));
+  }
+}
